1346e8b904a7c1a900a0fa2b78f9c9b41c417462
teru <s266126@wakayama-u.ac.jp>
2025-05-20 04:35:43 +0900

    add user name registration function

--- app/api/line-webhook/route.ts
+++ app/api/line-webhook/route.ts
@@ -2,6 +2,8 @@
 
 import { NextRequest, NextResponse } from 'next/server';
 import { Client, WebhookEvent } from '@line/bot-sdk';
+import { supabase } from '@/lib/supabase';
+import { Console } from 'console';
 
 const client = new Client({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN!,
@@ -17,17 +19,32 @@ export async function POST(req: NextRequest) {
       events.map(async (event) => {
         if (event.type === 'message' && event.message.type === 'text') {
           const userMessage = event.message.text;
+          const messageText = userMessage.trim();
+          const userId = event.source.userId;
 
           if (userMessage.startsWith('@シフト提出')) {
             await client.replyMessage(event.replyToken, {
               type: 'text',
               text: 'シフト提出はこちらのフォームからどうぞ！\nhttps://shift-management-linebot.vercel.app/',
             });
-          } else if (userMessage.startsWith('@名前')) {
-            await client.replyMessage(event.replyToken, {
+          } else if (userMessage.startsWith('@登録')) {
+            /*await client.replyMessage(event.replyToken, {
               type: 'text',
               text: '名前を登録しました（ダミー返信）',
-            });
+            }); */
+            const name = messageText.replace('@登録', '').trim();
+
+            if (name) {
+                //supabaseに名前登録
+                const { error } = await supabase.from('users').insert([{ name }]);
+
+                if ( error ) {
+                    console.error('supabaseへの登録エラー：', error.message);
+                } else {
+                    //console.log(`ユーザ名「${name}」を登録しました`)；；
+                }
+            }
+
           }
         }
 
